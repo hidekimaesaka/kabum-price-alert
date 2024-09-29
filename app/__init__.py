38c176bd88c63aa1c636ccdbdde09b4656a03199
hidekimsk <Lucasmsk11@gmail.com>
2024-09-29 20:33:38 -0300

    add: cors treatment and requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,13 @@ from app import scheduler
 from app.routes import user
 from app.routes import product
 
+from app.extensions import cors
 
 def create_app():
 
     app = Flask(__name__)
+
+    cors.init(app)
 
     user.init(app)
     product.init(app)
